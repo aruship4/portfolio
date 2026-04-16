b1bac71d0890f35767a4dde297d044ec73ff76b8
Arushi Patra <apatra@ucsd.edu>
2026-04-16 14:19:23 -0700

    Update global.js

--- global.js
+++ global.js
@@ -16,8 +16,17 @@ let pages = [
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
-    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
   }
+
+  const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
+
+url = !url.startsWith('http') ? BASE_PATH + url : url;
 
 // let navLinks = $$("nav a");
 // console.log(navLinks);
