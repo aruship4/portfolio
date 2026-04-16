0a4765fff1f0f6f1c4d240e18ab103b5ed29a391
Arushi Patra <apatra@ucsd.edu>
2026-04-16 14:11:23 -0700

    remove nav

--- global.js
+++ global.js
@@ -3,3 +3,27 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'resume/', title: 'Resume' }
+  ];
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  }
+
+// let navLinks = $$("nav a");
+// console.log(navLinks);
+
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname,
+//   );
+
+// currentLink?.classList.add("current");
