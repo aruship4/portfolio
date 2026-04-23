c1e8f0ec310e15f91310174dfa5d3f99d06a4e73
Arushi Patra <apatra@ucsd.edu>
2026-04-23 15:55:00 -0700

    formatting stats

--- global.js
+++ global.js
@@ -127,3 +127,8 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
 
   }
 }
+
+
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
