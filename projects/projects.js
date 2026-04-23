0d9968b0a91657aa34105c1f3398dfc59f6be537
Arushi Patra <apatra@ucsd.edu>
2026-04-23 15:25:07 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -2,6 +2,12 @@ import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
 
+const titleElement = document.querySelector('.projects-title');
+if (titleElement) {
+  titleElement.textContent = `Projects (${projects.length})`;
+}
+
+
 const projectsContainer = document.querySelector('.projects');
 
 renderProjects(projects, projectsContainer, 'h2');
