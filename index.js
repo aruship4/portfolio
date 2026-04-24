f439e0688d7a6a0c61b942e5a9a0f4dab6289dff
Arushi Patra <apatra@ucsd.edu>
2026-04-23 18:46:49 -0700

    title2

--- index.js
+++ index.js
@@ -13,12 +13,9 @@ const profileStats = document.querySelector('#profile-stats');
 
 if (profileStats) {
     profileStats.innerHTML = `
-          <h2>Github Stats</h2>
-          <dl>
             <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
             <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
             <dt>Followers:</dt><dd>${githubData.followers}</dd>
             <dt>Following:</dt><dd>${githubData.following}</dd>
-          </dl>
       `;
   }
