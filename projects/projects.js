a61c5cb638d298003f3acd821ed8310423ddebc9
Arushi Patra <apatra@ucsd.edu>
2026-04-30 21:38:35 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -8,51 +8,20 @@ if (titleElement) {
   titleElement.textContent = `${projects.length} Projects`;
 }
 
-
 const projectsContainer = document.querySelector('.projects');
-
 renderProjects(projects, projectsContainer, 'h2');
 
-
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-
-let arc = arcGenerator({
-  startAngle: 0,
-  endAngle: 2 * Math.PI,
-});
-
-d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
-
-let data = [1, 2];
-
-let total = 0;
-
-for (let d of data) {
-  total += d;
-}
-
-let angle = 0;
-let arcData = [];
-
-for (let d of data) {
-  let endAngle = angle + (d / total) * 2 * Math.PI;
-  arcData.push({ startAngle: angle, endAngle });
-  angle = endAngle;
-}
-
-let arcs = arcData.map((d) => arcGenerator(d));
+let sliceGenerator = d3.pie();
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
-arcs.forEach((arc, idx) => {
-  d3.select('svg')
-    .append('path')
-    .attr('d', arc)
-    .attr('fill', colors[idx]) // Fill in the attribute for fill color via indexing the colors variable
-})
-
-let data = [1, 2];
-let sliceGenerator = d3.pie();
+let data = [1, 2, 3, 4, 5, 5];
 let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
 
-let data = [1, 2, 3, 4, 5, 5];
+arcs.forEach((arc, idx) => {
+  d3.select('#projects-pie-plot')
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', colors(idx));
+});
