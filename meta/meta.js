9af31ca3c0616b894f8e193b5b1b0ef569e224c2
Arushi Patra <apatra@ucsd.edu>
2026-05-21 12:17:02 -0700

    Update meta.js

--- meta/meta.js
+++ meta/meta.js
@@ -221,6 +221,8 @@ function updateScatterPlot(data, commits) {
   const [minLines, maxLines] = d3.extent(commits, (d) => d.totalLines);
   const rScale = d3.scaleSqrt().domain([minLines, maxLines]).range([2, 30]);
 
+  const xAxis = d3.axisBottom(xScale);
+  
   const xAxisGroup = svg.select('g.x-axis');
   xAxisGroup.selectAll('*').remove();
   xAxisGroup.call(xAxis);
