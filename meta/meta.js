3f944cf6ed046ccf6d3d80b92b4582bf98c6a397
Arushi Patra <apatra@ucsd.edu>
2026-05-22 12:10:36 -0700

    file

--- meta/meta.js
+++ meta/meta.js
@@ -201,6 +201,34 @@ const timeDisplay = document.getElementById('commit-slider-time');
 
 let filteredCommits = commits;
 
+
+
+function updateFileDisplay(filteredCommits){
+  let lines = filteredCommits.flatMap((d) => d.lines);
+  let files = d3
+  .groups(lines, (d) => d.file)
+  .map(([name, lines]) => {
+    return { name, lines };
+  });
+  let filesContainer = d3
+  .select('#files')
+  .selectAll('div')
+  .data(files, (d) => d.name)
+  .join(
+    // This code only runs when the div is initially rendered
+    (enter) =>
+      enter.append('div').call((div) => {
+        div.append('dt').append('code');
+        div.append('dd');
+      }),
+  );
+
+  // This code updates the div info
+  filesContainer.select('dt > code').text((d) => d.name);
+  filesContainer.select('dd').text((d) => `${d.lines.length} lines`);
+
+}
+
 function updateScatterPlot(data, commits) {
   const width = 1000;
   const height = 600;
@@ -264,6 +292,8 @@ function onTimeSliderChange() {
   filteredCommits = commits.filter((d) => d.datetime <= commitMaxTime);
 
   updateScatterPlot(data, filteredCommits);
+
+  updateFileDisplay(filteredCommits);
 
   d3.select('#stats').html('');
   renderCommitInfo(data, filteredCommits);
